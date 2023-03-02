b8e0e372a7830c80b32079c58943cc24dd76e6f3
Ragna022 <gideonite4christ2017@gmail.com>
2023-03-02 15:01:19 +0100

    new

--- Word Puzzle/Assets/Scripts/GridSquare.cs
+++ Word Puzzle/Assets/Scripts/GridSquare.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameEvents;
 
 public class GridSquare : MonoBehaviour
 {
@@ -10,18 +11,93 @@ public class GridSquare : MonoBehaviour
     private AlphabetData.LetterData _correctLetterData;
 
     private SpriteRenderer _displayedImage;
+    private bool _selected;
+    private bool _clicked;
+    private int _index = -1;
+
+    public void SetIndex (int index)
+    {
+        _index = index;
+    }
+
+    public int GetIndex ()
+    {
+        return _index;
+    }
     
     void Start()
     {
         _displayedImage = GetComponent<SpriteRenderer>();
+        _selected = false;
+        _clicked = false;
+    }
+    private void OnEnable()
+    {
+        GameEvents.OnEnableSquareSelection += OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection += OnDisableSquareSelection;
+        GameEvents.OnSelectSquare += SelectSquare;
     }
 
-   public void SetSprite(AlphabetData.LetterData normalLetterData, AlphabetData.LetterData selectedLetterData, AlphabetData.LetterData correctLetterData)
+
+    private void OnDisable()
+    {
+        GameEvents.OnEnableSquareSelection -= OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection -= OnEnableSquareSelection;
+        GameEvents.OnSelectSquare -= SelectSquare;
+    }
+
+    public void OnEnableSquareSelection()
+    {
+        _clicked = true;
+        _selected = false;
+    }
+
+    public void OnDisableSquareSelection()
+    {
+        _clicked = false;
+        _selected = false;
+    }
+
+    public void SelectSquare( Vector3 position)
+    {
+        if (this.gameObject.transform.position == position)
+            _displayedImage.sprite = _selectedLetterData.image;
+    }
+
+    public void SetSprite(AlphabetData.LetterData normalLetterData, AlphabetData.LetterData selectedLetterData, AlphabetData.LetterData correctLetterData)
     {
         _normalLetterData = normalLetterData;
         _selectedLetterData = selectedLetterData;
         _correctLetterData = correctLetterData;
 
         GetComponent<SpriteRenderer>().sprite = _normalLetterData.image;
+    }
+
+
+    private void OnMouseDown()
+    {
+        OnEnableSquareSelection();
+        GameEvents.EnableSquareSelectionMethod();
+        CheckSquare();
+    }
+
+    private void OnMouseEnter()
+    {
+        CheckSquare();
+    }
+
+    private void OnMouseUp()
+    {
+        GameEvents.ClearSelectionMethod();
+        GameEvents.DisableSquareSelectionMethod();
+    }
+
+    private void CheckSquare()
+    {
+        if(_selected == false && _clicked == true)
+        {
+            _selected = true;
+            GameEvents.CheckSquareMethod(_normalLetterData.letter, gameObject.transform.position, _index);
+        }
     }
 }
